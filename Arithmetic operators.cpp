355b84f246b8ab40a7fd9aeed14c9e9cf67ec2d3
lakshya0102-dev <lakshya26102007@gmail.com>
2026-06-07 22:29:43 +0530

    Arithmetic operators

---
+++ Arithmetic operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10, b = 5;
+    cout << "First number: " << a << endl;
+    cout << "Second number: " << b << endl;
+
+    cout << "\nAddition: " << a + b << endl;
+    cout << "\nSubtraction: " << a - b << endl;
+    cout << "\nMultiplication: " << a * b << endl;
+    cout << "\nDivision: " << a / b << endl;
+    cout << "\nModulus: " << a % b << endl;
+
+    return 0;
+}
