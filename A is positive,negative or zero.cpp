7f8502fae3d95be4d9ed081c24f814258c2061f7
lakshya0102-dev <lakshya26102007@gmail.com>
2026-06-10 14:22:09 +0530

    Check wheather a is positive, negative or zero

---
+++ A is positive,negative or zero.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout << "Enter a number: ";
+    cin >> a;
+
+    if(a > 0){
+        cout << "The number is positive." << endl;
+    }
+    else if(a < 0){
+        cout << "The number is negative." << endl;
+    }
+    else{
+        cout << "The number is zero." << endl;
+    }
+
+    return 0;
+}
